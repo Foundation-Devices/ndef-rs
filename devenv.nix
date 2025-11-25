41cd5310d20409319879444d47957740886740c8
Georges Palauqui <georges@foundationdevices.com>
2025-11-25 11:19:25 +0100

    switch to devenv

---
+++ devenv.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
